5b2ef858ea3ecb7a8e04ffb9c2b2dad76a1b0dec
SylvainJunca <sylvain.junca@gmail.com>
2018-12-04 12:14:38 -0500

    Exercise 1 done

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,15 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+class Stores < ActiveRecord::Base
+  
+end
+
+
+puts "Creating Stores"
+Stores.create!(name: "Burnaby", annual_revenue: 30000, mens_apparel: true, womens_apparel: true)
+Stores.create!(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+Stores.create!(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts "number of stores : #{Stores.count}"
